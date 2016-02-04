69733e8a0b8d6a511cb9e7e983d0f8991f257e58
wangliang <txwg4011@outlook.com>
2016-02-04 16:07:50 +0800

    app.js second commit

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
- 
+ var express = require('express');
+ var port = process.env.PORT || 3000
+ var app = express();
+ app.set('views', './')  // views在本目录
+ app.set('view engine', 'jade');
+ app.listen(port);
+
+ console.log('imooc started on part 3000'); 
